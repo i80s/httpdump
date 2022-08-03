784ca6efb117b82abec36611bc0889a8f405088a
Justin Liu <rssnsj@gmail.com>
2022-08-03 13:11:04 +0800

    Use more proper variable names

--- print-http.c
+++ print-http.c
@@ -38,9 +38,9 @@ void http_print(const u_char *sp, u_int length)
 	if (strncmp(sp, "GET /", 5) == 0 || strncmp(sp, "POST /", 6) == 0) {
 		const char *req_start = sp, *req_end = sp + length;
 		const char *ln_start, *ln_end;
-		const char *host_start = NULL, *path_start = NULL,
-			*path_end, *method_start = NULL, *rf_start = NULL, *ua_start = NULL;
-		size_t path_len = 0, host_len = 0, method_len = 0, rf_len = 0, ua_len = 0;
+		const char *host_start = NULL, *uri_start = NULL,
+			*uri_end, *method_start = NULL, *rf_start = NULL, *ua_start = NULL;
+		size_t uri_len = 0, host_len = 0, method_len = 0, rf_len = 0, ua_len = 0;
 
 		/* Parse status line */
 		ln_start = req_start;
@@ -48,15 +48,15 @@ void http_print(const u_char *sp, u_int length)
 			return;
 		method_start = ln_start;
 		/* Pick out URL */
-		if ((path_start = (const char *)memchr(ln_start, ' ', ln_end - ln_start)) == NULL)
+		if ((uri_start = (const char *)memchr(ln_start, ' ', ln_end - ln_start)) == NULL)
 			return;
-		while (path_start < ln_end && *path_start == ' ') {
-			method_len = path_start - method_start;
-			path_start++;
+		while (uri_start < ln_end && *uri_start == ' ') {
+			method_len = uri_start - method_start;
+			uri_start++;
 		}
-		if ((path_end = (const char *)memchr(path_start, ' ', ln_end - path_start)) == NULL)
+		if ((uri_end = (const char *)memchr(uri_start, ' ', ln_end - uri_start)) == NULL)
 			return;
-		path_len = path_end - path_start;
+		uri_len = uri_end - uri_start;
 
 		/* Parse each header line */
 		for (ln_start = ln_end + 1; ln_start < req_end &&
@@ -65,13 +65,13 @@ void http_print(const u_char *sp, u_int length)
 			const char *nm_start, *nm_end;
 			const char *val_start, *val_end;
 			size_t nm_len, val_len;
-			
+
 			/* An empty line indicates header analysis is over */
 			if ((ln_end == ln_start) || (ln_end == ln_start + 1 && *ln_start == '\r')) {
 				ln_start = ln_end + 1;
 				break;
 			}
-			
+
 			/* Option name:: */
 			nm_start = ln_start;
 			if ((nm_end = (const char *)memchr(nm_start, ':', ln_end - nm_start)) == NULL)
@@ -82,7 +82,7 @@ void http_print(const u_char *sp, u_int length)
 			for (val_end = ln_end; (val_end > val_start) && is_space(*(val_end - 1)); val_end--);
 			if ((val_len = val_end - val_start) == 0)
 				continue;
-			
+
 			if (nm_len == 4 && strncasecmp(nm_start, "Host", 4) == 0) {
 				const char *sep;
 				/* Remember Host value */
@@ -98,18 +98,18 @@ void http_print(const u_char *sp, u_int length)
 				ua_len = val_len;
 			}
 		} /* for(ln_start = ln_end + 1;... */
-		
+
 		/* Check hostname validity */
 		if (host_start == NULL) {
 			host_start = null_host;
 			host_len = strlen(null_host);
 		}
-		
+
 		/* Print the whole URL if it has one. */
 		do {
-			char the_method[64] = "---", the_url[4096] = "", *filter;
+			char the_method[64] = "---", whole_url[4096] = "", *filter;
 
-			if (host_len + path_len >= sizeof(the_url))
+			if (host_len + uri_len >= sizeof(whole_url))
 				break;
 
 			if (method_len > 0) {
@@ -117,17 +117,17 @@ void http_print(const u_char *sp, u_int length)
 				the_method[method_len] = '\0';
 			}
 
-			memcpy(the_url, host_start, host_len);
-			memcpy(the_url + host_len, path_start, path_len);
-			the_url[host_len + path_len] = '\0';
+			memcpy(whole_url, host_start, host_len);
+			memcpy(whole_url + host_len, uri_start, uri_len);
+			whole_url[host_len + uri_len] = '\0';
 
 			/* If filter specified, check it */
 			if ((filter = getenv("FILTER"))) {
-				if (!strstr(the_url, filter))
+				if (!strstr(whole_url, filter))
 					break;
 			}
 
-			fprintf(stderr, "%-4s http://%s\n", the_method, the_url);
+			fprintf(stderr, "%-4s http://%s\n", the_method, whole_url);
 			//fflush(stderr);
 		} while (0);
 	} else if (strncmp(sp, "HTTP/1.", 7) == 0) {
@@ -149,7 +149,7 @@ void http_print(const u_char *sp, u_int length)
 		ln_start = resp;
 		if ((ln_end = (const char *)memchr(ln_start, '\n', resp_end - ln_start)) == NULL)
 			return;
-	
+
 		/* Pick out status code number */
 		if ((sc_start = (const char *)memchr(ln_start, ' ', ln_end - ln_start)) == NULL)
 			return;
@@ -163,7 +163,7 @@ void http_print(const u_char *sp, u_int length)
 		sd_start = sc_end + 1;
 		while (sd_start < ln_end && *sd_start == ' ')
 			sd_start++;
-		for (sd_end = ln_end; (sd_end > sd_start) && is_space(*(sd_end - 1)); sd_end--);
+		for (sd_end = ln_end; sd_end > sd_start && is_space(*(sd_end - 1)); sd_end--);
 		sd_len = sd_end - sd_start;
 
 		/* Convert status to numeric for validition. */
@@ -183,7 +183,7 @@ void http_print(const u_char *sp, u_int length)
 			size_t nm_len, val_len;
 
 			/* An empty line indicates header analysis is over */
-			if ((ln_end == ln_start) || (ln_end == ln_start + 1 && *ln_start == '\r')) {
+			if (ln_end == ln_start || (ln_end == ln_start + 1 && *ln_start == '\r')) {
 				ln_start = ln_end + 1;
 				break;
 			}
@@ -194,7 +194,7 @@ void http_print(const u_char *sp, u_int length)
 			nm_len = nm_end - nm_start;
 			/* Option value:: */
 			for (val_start = nm_end + 1; val_start < ln_end && is_blank(*val_start); val_start++);
-			for (val_end = ln_end; (val_end > val_start) && is_space(*(val_end - 1)); val_end--);
+			for (val_end = ln_end; val_end > val_start && is_space(*(val_end - 1)); val_end--);
 			if ((val_len = val_end - val_start) == 0)
 				continue;
 
